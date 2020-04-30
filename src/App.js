839e605f46462a23af306a707db9568c78ebfac1
catalunha <catalunha.mj@gmail.com>
2020-04-30 18:47:33 -0300

    aula60

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      token: 'Carregando...',
+      nome: '',
+      idade: ''
     };
     const firebaseConfig = {
       apiKey: "AIzaSyCr5mCRt9bGwobghuPfnWQ-sxEUt0g5GiE",
@@ -20,12 +22,40 @@ export default class App extends Component {
     // Initialize Firebase
     if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig);
-  }
+    }
+    //get any update =  Olheiro
+    // firebase.database().ref('token').on('value', (snapshot) => {
+    //   let state = this.state
+    //   state.token = snapshot.val()
+    //   this.setState(state)
+    // })
+    //get one time
+    firebase.database().ref('token').once('value').then((snapshot) => {
+      let state = this.state
+      state.token = snapshot.val()
+      this.setState(state)
+    })
+    // firebase.database().ref('usuarios').child(1).child('nome').on('value',(snapshot)=>{
+    //   let state = this.state
+    //   state.nome = snapshot.val()
+    //   this.setState(state)
+    // })
+    firebase.database().ref('usuarios').child(1).on('value', (snapshot) => {
+      let state = this.state
+      state.nome = snapshot.val().nome
+      state.idade = snapshot.val().idade
+      this.setState(state)
+    })
   }
 
   render() {
+    const { token, nome, idade } = this.state
     return (
-      <div>rjs123</div>
+      <div>
+        <h1>Token: {token}</h1>
+        <h1>Nome: {nome}</h1>
+        <h1>Idade: {idade}</h1>
+      </div>
     )
   }
 }
